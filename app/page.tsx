bdeffaddb9eaa04b32e0d30698b33e9357f09fdd
PeterAdam <adampeter1998@yahoo.com>
2023-11-04 22:24:51 +0100

    feat: adding authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main>
-      <h1>Learnweb</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }

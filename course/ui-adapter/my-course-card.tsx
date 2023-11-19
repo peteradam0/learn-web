27c257d1712a3abf1c75cd121249b1fa3e3c0332
peteradam0 <peter.adam.external@adesso.de>
2023-11-19 08:37:06 +0100

    feat: add status chip for my courses

--- course/ui-adapter/my-course-card.tsx
+++ course/ui-adapter/my-course-card.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, Chip, Image } from "@nextui-org/react";
 import React from "react";
 
 export default function MyCourseCard({ course }: any) {
@@ -7,21 +7,24 @@ export default function MyCourseCard({ course }: any) {
     <div className="shadow-lg">
       <Card className="py-4 pb-1" style={{ height: "300px" }}>
         <CardHeader
-          className="pb-0 pt-2 px-4 flex-col items-start"
-          style={{ height: "70px" }}
+          className="pb-0 px-4 flex-col items-start"
+          style={{ height: "60px" }}
         >
-          <p className="text-tiny uppercase font-bold">{course.category}</p>
-          <small className="text-default-500">{course.description}</small>
           <h4 className="font-bold text-large">{course.title}</h4>
+          <small className="text-default-500">{course.description}</small>
         </CardHeader>
         <CardBody className="overflow-visible py-2">
           <img
             alt="Card background"
             width={500}
-            style={{ height: "190px" }}
+            style={{ height: "170px" }}
             className="object-cover rounded-xl"
             src={course.imageUrl}
           />
+          <div style={{ paddingTop: "10px" }}>
+            {course.published && <Chip color="success">Published</Chip>}
+            {!course.published && <Chip color="warning">Unpublished</Chip>}
+          </div>
         </CardBody>
       </Card>
     </div>
--- course/ui-adapter/my-course-page.tsx
+++ course/ui-adapter/my-course-page.tsx
@@ -33,7 +33,6 @@ export default function MyCoursePage() {
 
   if (isLoading) return <p>Loading...</p>;
 
-  console.log(courseData);
   return (
     <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
       <div className="container max-w-screen-lg mx-auto">

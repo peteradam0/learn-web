548a7f20f18b32a073747a3db04a1b7540c2f7e5
peteradam0 <adampeter1998@yahoo.com>
2023-12-26 12:50:43 +0200

    feat: display suggestions from canvas

--- organizations/ui-adapter/add-user-to-organization-form.tsx
+++ organizations/ui-adapter/add-user-to-organization-form.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { Button, Input } from "@nextui-org/react";
+import {
+  Autocomplete,
+  AutocompleteItem,
+  Avatar,
+  Button,
+  Input,
+  Select,
+  SelectItem,
+} from "@nextui-org/react";
 
 import { SubmitHandler, useForm } from "react-hook-form";
 import { sendOrganizationMemberInvite } from "../api-adapter/create-organization";
@@ -13,6 +21,8 @@ export default function AddUserToOrganizationForm({
   onClose,
   organizationName,
 }: any) {
+  const [suggestedUsers, setSuggestedUsers] = useState([]);
+
   useEffect(() => {
     const canvasToken = Cookies.get("canvas_token");
     if (canvasToken) {
@@ -22,7 +32,11 @@ export default function AddUserToOrganizationForm({
 
   const getData = async (canvasToken: string) => {
     try {
-      await getOrganizationMemberSuggestions(organizationName, canvasToken);
+      const res = await getOrganizationMemberSuggestions(
+        organizationName,
+        canvasToken
+      );
+      setSuggestedUsers(res?.data);
     } catch (e) {
       console.log(e);
     }
@@ -55,12 +69,23 @@ export default function AddUserToOrganizationForm({
             <h3 className="text-default-500 text-small pb-1">
               Please provide the eamil address of the new member
             </h3>
-            <Input
-              label="email"
+            <Autocomplete
+              allowsCustomValue
+              className="max-w-xs"
+              defaultItems={suggestedUsers}
+              label="Search or type in email"
+              variant="bordered"
               {...register("email", {
                 required: "Email is required",
               })}
-            />
+            >
+              {(item: any) => (
+                <AutocompleteItem key={item?.email}>
+                  {item?.email}
+                </AutocompleteItem>
+              )}
+            </Autocomplete>
+
             {errors.email?.message && (
               <p className="text-sm text-red-400">{errors.email.message}</p>
             )}
@@ -81,3 +106,64 @@ export default function AddUserToOrganizationForm({
     </>
   );
 }
+
+const animals = [
+  {
+    label: "Cat",
+    value: "cat",
+    description: "The second most popular pet in the world",
+  },
+  {
+    label: "Dog",
+    value: "dog",
+    description: "The most popular pet in the world",
+  },
+  {
+    label: "Elephant",
+    value: "elephant",
+    description: "The largest land animal",
+  },
+  { label: "Lion", value: "lion", description: "The king of the jungle" },
+  { label: "Tiger", value: "tiger", description: "The largest cat species" },
+  {
+    label: "Giraffe",
+    value: "giraffe",
+    description: "The tallest land animal",
+  },
+  {
+    label: "Dolphin",
+    value: "dolphin",
+    description: "A widely distributed and diverse group of aquatic mammals",
+  },
+  {
+    label: "Penguin",
+    value: "penguin",
+    description: "A group of aquatic flightless birds",
+  },
+  {
+    label: "Zebra",
+    value: "zebra",
+    description: "A several species of African equids",
+  },
+  {
+    label: "Shark",
+    value: "shark",
+    description:
+      "A group of elasmobranch fish characterized by a cartilaginous skeleton",
+  },
+  {
+    label: "Whale",
+    value: "whale",
+    description: "Diverse group of fully aquatic placental marine mammals",
+  },
+  {
+    label: "Otter",
+    value: "otter",
+    description: "A carnivorous mammal in the subfamily Lutrinae",
+  },
+  {
+    label: "Crocodile",
+    value: "crocodile",
+    description: "A large semiaquatic reptile",
+  },
+];

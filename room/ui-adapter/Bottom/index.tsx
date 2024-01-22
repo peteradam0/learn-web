c2d7c1d34c034d08522d074e11af2c735375a548
peteradam0 <adampeter1998@yahoo.com>
2024-01-22 12:50:56 +0100

    feat: ui improvments for video room

--- room/ui-adapter/Bottom/index.tsx
+++ room/ui-adapter/Bottom/index.tsx
@@ -1,3 +1,4 @@
+import { Tab, Tabs, Tooltip } from "@nextui-org/react";
 import cx from "classnames";
 import { Mic, Video, PhoneOff, MicOff, VideoOff } from "lucide-react";
 
@@ -9,22 +10,55 @@ const Bottom = (props: any) => {
       className="flex gap-3 items-center"
       style={{
         width: "100%",
-        background: "grey",
-
         paddingRight: "10%",
       }}
     >
-      {muted ? (
-        <MicOff size={30} onClick={toggleAudio} />
-      ) : (
-        <Mic size={30} onClick={toggleAudio} />
-      )}
-      {playing ? (
-        <Video size={30} onClick={toggleVideo} />
-      ) : (
-        <VideoOff size={30} onClick={toggleVideo} />
-      )}
-      <PhoneOff size={30} onClick={leaveRoom} />
+      <div className="flex" style={{ paddingLeft: "40%" }}>
+        <Tabs aria-label="Options" color="primary" variant="bordered">
+          <Tab
+            key="photos"
+            title={
+              <div className="flex items-center space-x-2">
+                {muted ? (
+                  <Tooltip content="Mute">
+                    <MicOff size={30} onClick={toggleAudio} />
+                  </Tooltip>
+                ) : (
+                  <Tooltip content="Unmute">
+                    <Mic size={30} onClick={toggleAudio} />
+                  </Tooltip>
+                )}
+              </div>
+            }
+          />
+          <Tab
+            key="music"
+            title={
+              <div className="flex items-center space-x-2">
+                {playing ? (
+                  <Tooltip content="Video off">
+                    <Video size={30} onClick={toggleVideo} />
+                  </Tooltip>
+                ) : (
+                  <Tooltip content="Video on">
+                    <VideoOff size={30} onClick={toggleVideo} />
+                  </Tooltip>
+                )}
+              </div>
+            }
+          />
+          <Tab
+            key="videos"
+            title={
+              <div className="flex items-center space-x-2">
+                <Tooltip content="Leave">
+                  <PhoneOff size={30} onClick={leaveRoom} />
+                </Tooltip>
+              </div>
+            }
+          />
+        </Tabs>
+      </div>
     </div>
   );
 };

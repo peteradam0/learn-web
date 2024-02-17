1d668f6e5b7c88ace412ba8e5dc13f91a50369e8
PeterAdam-ad <adampeter1998@yahoo.com>
2024-02-17 09:39:36 +0100

    feat: add searchbar on public course page

--- course/ui-adapter/course-page.tsx
+++ course/ui-adapter/course-page.tsx
@@ -5,14 +5,21 @@ import CourseCard from "./course-card";
 import { getUserToken } from "../domain/get-user-token";
 import { redirect } from "next/navigation";
 import { getSelfhedCourses } from "@/dashboard/api-adapter/get-published-courses";
+import { SearchBar } from "@/course/ui-adapter/searchbar";
 
 export default function CoursePage() {
   const [courseData, setCourseData] = useState<any>([]);
   const [isLoading, setLoading] = useState(true);
+  const [displayData, setDisplayData] = useState<any>([]);
+  const [search, setSerach] = useState("");
 
   useEffect(() => {
     getCourseData();
   }, []);
+
+  useEffect(() => {
+    console.log(search);
+  }, [search]);
 
   const getCourseData = async () => {
     const token = await getUserToken();
@@ -35,18 +42,26 @@ export default function CoursePage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="container max-w-screen-lg mx-auto">
-        <h2 className="font-semibold text-xl text-white  pb-1">
-          Frontend & Fullstack Engineering Courses
-        </h2>
-        <p className="text-gray-500 mb-6">
-          ed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium.
-        </p>
-
-        <div className="grid grid-cols-4 gap-4 pb-2">
-          {courseData.map((course: any) => (
-            <CourseCard key={course.id} course={course} />
-          ))}
+        <div style={{ marginBottom: "3%" }}>
+          <h2 className="font-semibold text-xl text-white  pb-1">
+            Frontend & Fullstack Engineering Courses
+          </h2>
+          <p className="text-gray-500 mb-6">
+            ed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium.
+          </p>
+        </div>
+        <SearchBar setSearch={setSerach} />
+        <div className="grid grid-cols-4  pb-2" style={{ gap: "1.6rem" }}>
+          {courseData
+            .filter((course: any) => {
+              return search.toLocaleLowerCase() === ""
+                ? course
+                : course.title.toLocaleLowerCase().includes(search);
+            })
+            .map((course: any) => (
+              <CourseCard key={course.id} course={course} />
+            ))}
         </div>
       </div>
     </div>
---
+++ course/ui-adapter/searchbar.tsx
@@ -0,0 +1,19 @@
+import { SearchIcon } from "@/course/ui-adapter/search-icon";
+import { Input } from "@nextui-org/react";
+import { Form } from "react-hook-form";
+
+export const SearchBar = ({ setSearch }: any) => {
+  return (
+    <div style={{ marginBottom: "5%", paddingLeft: "2px", width: "30%" }}>
+      <Input
+        type="email"
+        radius="sm"
+        variant="underlined"
+        onChange={(e) => setSearch(e.target.value)}
+        startContent={
+          <SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+        }
+      />
+    </div>
+  );
+};

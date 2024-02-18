78c63a32bf145f25e958cd557d9516307ac91191
peteradam0 <adampeter1998@yahoo.com>
2024-02-18 10:44:55 +0100

    feat: add prittier config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParens: 'avoid',
+    slingeQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabWidth: 2,
+    trailingComma: 'none'
+}
+
+module.exports = options

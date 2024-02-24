c0a1044499e3e7b6ebe1974dd40f0a5920bf35b4
PeterAdam-ad <adampeter1998@yahoo.com>
2024-02-24 11:47:27 +0100

    fix: re-trigger deployment

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,38 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs"
 
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 
 export default authMiddleware({
   publicRoutes: ["/api/uploadthing", "/test", "/api/socket", "/api/token"],
 
   async afterAuth(auth, req) {
     if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
+      return redirectToSignIn({ returnBackUrl: req.url })
     }
 
     if (req.nextUrl.pathname.includes("/administration")) {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user`, {
         method: "GET",
         headers: {
-          Authorization: `Bearer ${req.cookies.get("__session")?.value}`,
-        },
-      });
+          Authorization: `Bearer ${req.cookies.get("__session")?.value}`
+        }
+      })
 
-      const userData = await res.json();
+      const userData = await res.json()
 
       if (userData.userRole !== "ADMIN") {
-        return NextResponse.redirect(new URL("/", req.url));
+        return NextResponse.redirect(new URL("/", req.url))
       }
     }
 
     // If the user is logged in and trying to access a protected route, allow them to access route
     if (auth.userId && !auth.isPublicRoute) {
-      return NextResponse.next();
+      return NextResponse.next()
     }
-    // Allow users visiting public routes to access them
-    return NextResponse.next();
-  },
-});
+    return NextResponse.next()
+  }
+})
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"]
+}

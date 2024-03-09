21077dfd0b9c0a4add9772f6a5cb83a5990c3db3
peteradam0 <adampeter1998@yahoo.com>
2024-03-09 21:46:19 +0100

    refactor: remove page component

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,16 @@
-import { getCoursesDomain as getCoursesDomain } from "@/course/api/get-courses-domain"
+import { getCoursesDomain } from "@/course/api/get-courses-domain"
 import { getInProgressCourseDomain } from "@/course/api/get-in-progress-courses"
 import { queryToken } from "@/course/api/query/get-user-token"
 import { getUserDomainData } from "@/dashboard/api/get-user-domain-data"
-import { DashboardPage } from "@/dashboard/ui/dashboard-page"
+import { AllCoursesCard } from "@/dashboard/ui/all-courses-card"
+import { CoursesInProgressCard } from "@/dashboard/ui/courses-in-progress-card"
 import { getActiveEventsDomain } from "@/event/api/get-active-events"
 import { getFutureEvents } from "@/event/api/get-future-events"
+import { ActiveEventsCard } from "@/event/ui/active-events-card"
+import { UpcomingEvents } from "@/event/ui/upcoming-events-card"
+import { Footer } from "@/navigation/ui/footer"
+import MainHeader from "@/navigation/ui/main-navigation"
+import { Divider } from "@nextui-org/react"
 
 export default async function DashboardPageRoute() {
   const token = await queryToken()
@@ -17,13 +23,48 @@ export default async function DashboardPageRoute() {
 
   return (
     <>
-      <DashboardPage
-        user={user}
-        inProgressCourses={inProgressCourses}
-        courses={courses}
-        events={events}
-        futureEvents={futureEvents}
-      />
+      <MainHeader />
+      <div
+        style={{
+          marginLeft: "17%",
+          marginRight: "17%",
+          paddingTop: "2%",
+          paddingBottom: "4rem"
+        }}
+        className="primaryBackGround"
+      >
+        <h1 className="text-xl pb-7">Welcome Back, {user.username} </h1>
+        <Divider />
+        <div className="pt-5">
+          <div className="grid grid-cols-2" style={{ gap: "70px" }}>
+            <div>
+              <h1>Courses in progress</h1>
+              <div className="pt-5">
+                <CoursesInProgressCard inProgressCourses={inProgressCourses} />
+              </div>
+            </div>
+            <div>
+              <h1>Explore courses</h1>
+              <div className="pt-5">
+                <AllCoursesCard courses={courses} />
+              </div>
+            </div>
+            <div>
+              <h1>Active events</h1>
+              <div className="pt-5">
+                <ActiveEventsCard events={events} />
+              </div>
+            </div>
+            <div>
+              <h1>Upcoming events</h1>
+              <div className="pt-5">
+                <UpcomingEvents futureEvents={futureEvents} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   )
 }
